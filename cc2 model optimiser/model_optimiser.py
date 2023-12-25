7fe6c91a2d778f19ea724b11daaecb18759dfd3b
Chromatis <christian.loginmail@gmail.com>
2023-12-25 17:12:27 +0100

    Feat: Steamlined the conversion process

--- cc2 model optimiser/model_optimiser.py
+++ cc2 model optimiser/model_optimiser.py
@@ -1,11 +1,11 @@
+#Optimises blockbench structure models by connecting planes with the same texture
+#Also adds the display data and changes texture to pixel
+
 import json
+import sys
 import numpy
 import subprocess
 import os
-
-
-file_name = "model.json"
-
 
 def modify_binary_arr(texture_id,face,layer,x,y):
     if layer not in binary_array_dict[texture_id][face]:
@@ -19,8 +19,14 @@ def get_planes(bin_arr):
     p.stdin.flush()
     return p.stdout.read().split("\n")[:-1]
 
-rectangle_dissector_path = os.path.abspath('./cc2 model optimiser/dissector')
-model_path = os.path.abspath(os.path.join(os.getcwd(), 'cc2 model optimiser', file_name))
+file_name = input("Enter file name: ")
+
+rectangle_dissector_path = os.path.abspath('./dissector')
+model_path = os.path.abspath(os.path.join(os.getcwd(), file_name))
+
+if not os.path.exists(model_path):
+    print(f"Error: File at '{model_path}' not found.")
+    sys.exit(1)
 
 with open(model_path, 'r') as file:
     data = json.load(file)
@@ -30,12 +36,20 @@ print("Loaded {}".format(file_name))
 if 'credit' in data:
     del data['credit']
 
+if 'display' not in data:
+    data['display'] = {"head":{"translation":[0,12.4,0],"scale":[1.54,1.54,1.54]}}
+
+print("Splitting into texture layers")
+
 #Put Voxel into binary layers
 binary_array_dict = dict()
-for texture in data["textures"]:
-    binary_array_dict[texture] = dict()
+for texture_id, texture in data["textures"].items():
+
+    data["textures"][texture_id] = texture.replace("block/","pixel/pixel_").replace("_concrete","").replace("_planks","")
+
+    binary_array_dict[texture_id] = dict()
     for face in ["north", "south", "west", "east", "down", "up"]:
-        binary_array_dict[texture][face] = dict()
+        binary_array_dict[texture_id][face] = dict()
 
 for cube in data['elements']:
     texture_id = next(iter(cube["faces"].values()))["texture"][1:]
@@ -57,8 +71,6 @@ for cube in data['elements']:
             case "south":
                 modify_binary_arr(texture_id,"south",c,a,b)
 
-print("Finished conversion to binary array")
-
 new_elements = list()
 
 for texture, faces in binary_array_dict.items():
@@ -72,8 +84,8 @@ for texture, faces in binary_array_dict.items():
                 plane = plane.split(" ")
                 from1 = int(plane[0])*0.5
                 from2 = int(plane[1])*0.5
-                to1 = int(plane[2])*0.5+0.5
-                to2 = int(plane[3])*0.5+0.5
+                to1 = round(int(plane[2])*0.5+0.501,3)
+                to2 = round(int(plane[3])*0.5+0.501,3)
 
                 match face:
                     case "west":
